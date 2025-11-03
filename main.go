8f8f209e0b88a41ac298285ba9b9e59f54ff3055
Alessio Toniolo <atoniolo76@gmail.com>
2025-11-02 22:16:34 -0500

    get available instance types

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"toni/gotoni/pkg/client"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	apiToken := os.Getenv("LAMBDA_API_KEY")
+	c := http.Client{Timeout: time.Duration(5) * time.Second}
+
+	instances, err := client.GetAvailableInstanceTypes(&c, apiToken)
+	if err != nil {
+		fmt.Println("Error getting available instance types: ", err)
+		return
+	}
+	fmt.Println("Available instance types: ", instances)
 }
