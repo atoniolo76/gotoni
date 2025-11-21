69554d7ba9e414303fe062d69dc14dbc7ab5fccc
Alessio Toniolo <atoniolo76@gmail.com>
2025-11-20 19:23:36 -0800

    remove profiler

--- pkg/morinprofiler/test.go
+++
@@ -1,9 +0,0 @@
-package morinprofiler
-
-import (
-	"github.com/charmbracelet/lipgloss"
-)
-
-func RunMorinProfiler() {
-	lipgloss.NewStyle().Foreground(lipgloss.Color("#7D56F4")).Render("Hello, World!")
-}
