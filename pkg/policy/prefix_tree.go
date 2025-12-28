ac74f27661c1ca7bc74c866cefdc3429180a9214
Alessio Toniolo <atoniolo76@gmail.com>
2025-12-28 18:02:03 +0100

    Implement ternary trie

--- pkg/policy/prefix_tree.go
+++ pkg/policy/prefix_tree.go
@@ -1,15 +1,53 @@
 package policy
 
-import "github.com/atoniolo76/gotoni/pkg/client"
+import (
+	"github.com/atoniolo76/gotoni/pkg/client"
+)
 
 type Node struct {
-	text            string
-	storedInstances []client.RunningInstance
-	parent          Node
-	left            Node
-	right           Node
+	prefix          string
+	storedInstances []*client.RunningInstance
+	isEndOfWord     bool
+	low             *Node
+	equal           *Node
+	high            *Node
 }
 
-type PrefixTree struct {
-	nodes []Node
+type TernaryTree struct {
+	root *Node
+}
+
+func Insert(prefix string, instance *client.RunningInstance, root *Node) {
+	currentPrefix := prefix
+	currentRoot := root
+	for i := 0; i < len(prefix); i++ {
+		if currentRoot == nil {
+			currentRoot = &Node{
+				prefix:          currentPrefix,
+				storedInstances: []*client.RunningInstance{instance},
+				isEndOfWord:     true,
+				low:             nil,
+				equal:           nil,
+				high:            nil,
+			}
+		}
+
+		if currentRoot.prefix[i] > currentPrefix[i] {
+			currentPrefix = currentPrefix[:i]
+			currentRoot = currentRoot.low
+
+		} else if currentRoot.prefix[i] < currentPrefix[i] {
+			currentPrefix = currentPrefix[:i]
+			currentRoot = currentRoot.high
+
+		} else {
+			if i == len(prefix)-1 {
+				currentRoot.storedInstances = append(currentRoot.storedInstances, instance)
+				currentRoot.isEndOfWord = true
+			} else {
+				currentPrefix = currentPrefix[:i+1]
+				currentRoot = currentRoot.equal
+			}
+		}
+	}
 }
